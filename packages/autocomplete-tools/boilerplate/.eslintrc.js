f0b23aa6bb9065379ab222c3ad5c84d5f3a6187a
Sean Sullivan <ssullivan61198@gmail.com>
2021-09-17 16:56:24 -0700

    Fix lint deps

--- packages/autocomplete-tools/boilerplate/.eslintrc.js
+++ packages/autocomplete-tools/boilerplate/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
