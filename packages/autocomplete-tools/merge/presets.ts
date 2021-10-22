fa69fa8d682d59bf2b77217a7728f361a2cc8dc6
Federico Ciardi <fed.ciardi@gmail.com>
2021-10-22 12:35:39 +0200

    Add cobra and oclif presets

--- packages/autocomplete-tools/merge/presets.ts
+++ packages/autocomplete-tools/merge/presets.ts
@@ -3,6 +3,14 @@
 // can override the one of the old one. All the props not contained here are overridden
 // if and only if they are not present in the new spec at the same location
 
+export type Preset = {
+  commandProps: Set<string>;
+  optionProps: Set<string>;
+  argProps: Set<string>;
+};
+
+export type PresetName = keyof typeof presets;
+
 export const defaultPreset: Preset = {
   commandProps: new Set(["name", "description", "args", "subcommands", "options"]),
   optionProps: new Set(["name", "description", "args"]),
@@ -53,12 +61,21 @@ export const presets: Record<string, Preset> = {
       "default",
     ]),
   },
+  oclif: {
+    commandProps: new Set(["name", "description", "subcommands", "options", "args"]),
+    optionProps: new Set(["name", "description", "isRequired", "args", "exclusiveOn"]),
+    argProps: new Set(["name", "description", "isOptional", "suggestions", "default"]),
+  },
+  cobra: {
+    commandProps: new Set(["name", "description", "subcommands", "options", "args"]),
+    optionProps: new Set([
+      "name",
+      "description",
+      "displayName",
+      "isRepeatable",
+      "isRequired",
+      "args",
+    ]),
+    argProps: new Set(["name", "description", "template", "default"]),
+  },
 };
-
-export type Preset = {
-  commandProps: Set<string>;
-  optionProps: Set<string>;
-  argProps: Set<string>;
-};
-
-export type PresetName = keyof typeof presets;
