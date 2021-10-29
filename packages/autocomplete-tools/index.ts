6e91710e2201bc8ce7f6023a2fd7cfe98738c8f7
Federico Ciardi <fed.ciardi@gmail.com>
2021-10-29 11:21:57 +0200

    fix(tools): dynamic load commander integration only when in dev environment

--- packages/autocomplete-tools/index.ts
+++ packages/autocomplete-tools/index.ts
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { generateFigSpec } from "@withfig/commander";
 import merge from "./scripts/merge";
 import dev from "./scripts/dev";
 import createSpec from "./scripts/create-spec";
@@ -21,8 +19,13 @@ program.addCommand(dev);
 program.addCommand(merge);
 
 if (process.env.NODE_ENV === "development") {
-  program.command("generateFigSpec").action(() => {
-    generateFigSpec(program, "generated/autocomplete-tools-spec-new.ts");
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  import("@withfig/commander").then((module) => {
+    program.command("generateFigSpec").action(() => {
+      module.generateFigSpec(program, "generated/autocomplete-tools-spec-new.ts");
+    });
+    program.parse(process.argv);
   });
+} else {
+  program.parse(process.argv);
 }
-program.parse(process.argv);
