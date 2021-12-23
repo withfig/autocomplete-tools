76c63f6e4a420d1d2f9962ae1a07c8c4cf197295
Federico Ciardi <fed.ciardi@gmail.com>
2021-12-23 11:41:02 +0100

    feat: add `@withfig/autocomplete-generators` template

---
+++ packages/autocomplete-generators/src/index.ts
@@ -0,0 +1,87 @@
+export interface FilepathsOptions {
+  /** Show suggestions with any of these extensions. Do not include the leading dot. */
+  extensions?: string[];
+  /** Show suggestions that include this string */
+  includes?: string;
+  /** Show suggestions where the name exactly matches one of these strings */
+  equals?: string | string[];
+  /** Show suggestions where the name matches this expression */
+  matches?: RegExp;
+  /**
+   * Defines how folders are suggested.
+   *
+   * - **Default:** `always` will always suggest folders.
+   * - `filter` will treat folders like files, filtering based on the name.
+   * - `never` will never suggest folders.
+   */
+  suggestFolders?: "filter" | "always" | "never";
+  /**
+   * Set properties of suggestions of type "file".
+   */
+  editFileSuggestions?: Omit<Fig.Suggestion, "name" | "type">;
+  /**
+   * Set properties of suggestions of type "folder".
+   */
+  editFolderSuggestions?: Omit<Fig.Suggestion, "name" | "type">;
+}
+
+/**
+ * Sugar over using the `filepaths` template with `filterTemplateSuggestions`. If any of the
+ * conditions match, the suggestion will be accepted.
+ *
+ * Basic filepath filters can be replaced with this generator.
+ *
+ * @example
+ * ```
+ * // inside a `Fig.Arg`...
+ * generators: filepaths({ extensions: ["mjs", "js", "json"] });
+ * ```
+ */
+export function filepaths(options: FilepathsOptions): Fig.Generator {
+  const {
+    extensions = [],
+    includes,
+    equals = [],
+    matches,
+    suggestFolders = "always",
+    editFileSuggestions,
+    editFolderSuggestions,
+  } = options;
+  const extensionsSet = new Set(extensions);
+  const equalsSet = new Set(equals);
+  return {
+    template: "filepaths",
+    filterTemplateSuggestions: (suggestions = []) => {
+      const filtered = suggestions.filter(({ name = "", type }) => {
+        if (suggestFolders !== "filter" && type === "folder") {
+          return suggestFolders === "always";
+        }
+        if (equalsSet.has(name)) return true;
+        if (matches && matches.test(name)) return true;
+        if (includes && name.includes(includes)) return true;
+        // handle extensions
+        const [_, ...suggestionExtensions] = name.split(".");
+        if (suggestionExtensions.length >= 1) {
+          let i = suggestionExtensions.length - 1;
+          let stackedExtensions = suggestionExtensions[i];
+          do {
+            if (extensionsSet.has(stackedExtensions)) {
+              return true;
+            }
+            i -= 1;
+            // `i` may become -1 which is not a valid index, but the extensionSet check at the beginning is not run in that case,
+            // so the wrong extension is not evaluated
+            stackedExtensions = [suggestionExtensions[i], stackedExtensions].join(".");
+          } while (i >= 0);
+        }
+        return false;
+      });
+      if (!editFileSuggestions && !editFolderSuggestions) return filtered;
+
+      return filtered.map((suggestion) => ({
+        ...suggestion,
+        ...((suggestion.type === "file" ? editFileSuggestions : editFolderSuggestions) || {}),
+      }));
+    },
+  };
+}
