ecabd04cdaaa52227377d8a4d36f90fdf6f5094f
Federico Ciardi <fed.ciardi@gmail.com>
2022-05-05 19:49:21 +0200

    feat(create-completion-spec): do not create `user` subfolder

--- packages/create-completion-spec/src/index.ts
+++ packages/create-completion-spec/src/index.ts
@@ -6,24 +6,17 @@ import pc from "picocolors";
 
 export default function runProgram() {
   const boilerplateDir = path.resolve(__dirname, "..", "boilerplate");
-  const dirs = [
-    path.join(process.cwd(), ".fig", "autocomplete"),
-    path.join(process.cwd(), ".fig", "user", "autocomplete"),
-  ];
+  const dir = path.join(process.cwd(), ".fig", "autocomplete");
   console.log(pc.yellow("Copying boilerplate directory..."));
-  for (const dir of dirs) {
-    fs.mkdirSync(dir, { recursive: true });
-    execSync(`cp -a ${boilerplateDir}/. ${dir}`);
-  }
-  console.log(pc.green("Finished copying boilerplate directory."));
+  fs.mkdirSync(dir, { recursive: true });
+  execSync(`cp -a ${boilerplateDir}/. ${dir}`);
+  console.log(pc.green("Finished copying boilerplate directory"));
   console.log("----");
   console.log(pc.yellow("Installing npm deps..."));
-  for (const dir of dirs) {
-    try {
-      execSync("npm i", { cwd: dir });
-    } catch (error) {
-      continue;
-    }
+  try {
+    execSync("npm i", { cwd: dir });
+    console.log(pc.green("Finished installing npm deps"));
+  } catch {
+    console.log(pc.red("Error installing npm deps, please install them manually"));
   }
-  console.log(pc.green("Finished installing npm deps"));
 }
