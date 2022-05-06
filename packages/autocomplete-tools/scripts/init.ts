8f5237f4c9b40b8481da5c30dd593912ce0396ba
Federico Ciardi <fed.ciardi@gmail.com>
2022-05-06 21:20:46 +0200

    feat: improve `create-completion-spec`

--- packages/autocomplete-tools/scripts/init.ts
+++ packages/autocomplete-tools/scripts/init.ts
@@ -1,8 +1,8 @@
-import run from "create-completion-spec";
+import { createBoilerplateFolder } from "create-completion-spec";
 import { Command } from "commander";
 
 const program = new Command("init")
   .description("initialize fig custom spec boilerplate in current directory")
-  .action(run);
+  .action(createBoilerplateFolder);
 
 export default program;
