6d0eb0e644167244cd69ab7d9186598aeaffe8ae
Federico Ciardi <fed.ciardi@gmail.com>
2022-05-19 17:22:53 +0200

    ci: remove redundant node versions from lint workflow

--- shared/index.ts
+++ shared/index.ts
@@ -3,11 +3,4 @@ import { SpecMixin, applyMixin } from "./src/mixins";
 import { SpecLocationSource, makeArray } from "./src/utils";
 import type * as Internal from "./src/internal";
 
-export {
-  Internal,
-  convertSubcommand,
-  SpecMixin,
-  applyMixin,
-  makeArray,
-  SpecLocationSource
-};
+export { Internal, convertSubcommand, SpecMixin, applyMixin, makeArray, SpecLocationSource };
