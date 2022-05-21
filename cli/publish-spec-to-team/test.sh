21d3481aaf5c6a6d872b3cab66aeb2d2bb91baa3
Federico Ciardi <fed.ciardi@gmail.com>
2022-05-21 15:37:15 +0200

    chore: run pre-commit tests only on changed workspaces

--- cli/publish-spec-to-team/test.sh
+++ cli/publish-spec-to-team/test.sh
@@ -2,7 +2,8 @@
 
 # We had to extract this script to a file because yarn crashes when it finds a `!` in an npm script
 # We are using npm here because the pid returned by yarn is wrong and would not shut down the server 
-npm run test:server & SERVER_PID=$!
-sleep 2
-yarn test:fixtures
+npm run test:server & 
+SERVER_PID=$! &&
+sleep 2 && # Arbitrary time
+npm run test:fixtures
 kill $SERVER_PID
