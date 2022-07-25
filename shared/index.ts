a70bce908ee8b490bc9779f8abeba37df6b6be6d
Ilkin Bayramli <43158991+ibayramli@users.noreply.github.com>
2022-07-25 13:18:01 -0700

    Ilkin/mixins (#70)
    * refactor conversion in a more general way
    
    * dont know what I changed here
    
    * revert yarn files to main version
    
    * remove publish-spec-to-tea
    
    * revert package.json to the old version
    
    * remove yarn-1.22.19.cjs
    
    Co-authored-by: Sean Sullivan <ssullivan61198@gmail.com>

--- shared/index.ts
+++ shared/index.ts
@@ -1,20 +1,21 @@
 import type * as Internal from "./src/convert";
-import { convertSubcommand } from "./src/convert";
-
 import type * as Metadata from "./src/specMetadata";
+import { revertSubcommand } from "./src/revert";
+import { convertSubcommand } from "./src/convert";
 import { convertLoadSpec, initializeDefault } from "./src/specMetadata";
-
-import { SpecMixin, applyMixin } from "./src/mixins";
+import { SpecMixin, applyMixin, mergeSubcommands } from "./src/mixins";
 import { SpecLocationSource, makeArray } from "./src/utils";
 
 export {
   Internal,
+  revertSubcommand,
   convertSubcommand,
   Metadata,
   convertLoadSpec,
   initializeDefault,
   SpecMixin,
   applyMixin,
+  mergeSubcommands,
   makeArray,
   SpecLocationSource,
 };
