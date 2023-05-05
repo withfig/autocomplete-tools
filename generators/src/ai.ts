6628bc8e276825bc8756f67416892858e9e4a385
grant0417 <grantgurvis@gmail.com>
2023-05-04 23:13:33 -0700

    AI generator bump timeouts and add input limits

--- generators/src/ai.ts
+++ generators/src/ai.ts
@@ -4,26 +4,35 @@ export type GeneratorFn<T> = (args: {
   generatorContext: Fig.GeneratorContext;
 }) => Promise<T> | T;
 
+const MAX_TOKENS = 4097;
+const TOKEN_TO_CHAR_RATIO = 4;
+const MARGIN_RATIO = 0.8;
+const MAX_CHARS = MAX_TOKENS * TOKEN_TO_CHAR_RATIO * MARGIN_RATIO;
+
 /**
  * A generator that uses the Fig AI API to generate suggestions.
  *
  * @param prompt The prompt to use for the AI. Can be a string or a generator function.
  * @param message The message to send to the AI. Can be a string or a generator function.
  * @param postProcess A function to post-process the AI's response.
+ * @param temperature The temperature to use for the AI.
  * @returns A Fig generator.
  */
 export function ai({
+  name,
   prompt,
   message,
   postProcess,
   temperature,
 }: {
+  name: string;
   prompt?: string | GeneratorFn<string>;
   message: string | GeneratorFn<string>;
   postProcess?: (out: string) => Fig.Suggestion[];
   temperature?: number;
 }): Fig.Generator {
   return {
+    scriptTimeout: 15000,
     custom: async (tokens, executeShellCommand, generatorContext) => {
       const enabled = await executeShellCommand(
         "fig settings --format json autocomplete.ai.enabled"
@@ -56,8 +65,12 @@ export function ai({
             })
           : message;
 
+      const len = (promptString?.length ?? 0) + messageString.length;
+
       const body = {
         model: "gpt-3.5-turbo",
+        source: "autocomplete",
+        name,
         messages: [
           ...(promptString
             ? [
@@ -69,7 +82,7 @@ export function ai({
             : []),
           {
             role: "user",
-            content: messageString,
+            content: messageString.slice(0, MAX_CHARS - len),
           },
         ],
         temperature,
@@ -82,13 +95,13 @@ export function ai({
         `fig _ request --route /ai/chat --method POST --body '${escapedBodyJson}'`
       );
 
-      console.warn(res);
       const json = JSON.parse(res);
 
       const a =
         json?.choices
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           .map((c: any) => c?.message?.content)
-          .filter((c: any) => typeof c === "string")
+          .filter((c: unknown) => typeof c === "string")
           .map((c: string) => c.trim().replace(/\n/g, " "))
           .map((c: string) =>
             postProcess
