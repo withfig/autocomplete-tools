67c4e707715c1960293c79d1dda6450188271fc0
Grant Gurvis <grangurv@amazon.com>
2023-11-21 10:41:20 -0800

    fix tsx and eslint issue

--- cli/publish-spec/test.sh
+++ cli/publish-spec/test.sh
@@ -4,7 +4,7 @@ set -e
 
 # We had to extract this script to a file because pnpm crashes when it finds a `!` in an npm script
 # We are using npm here because the pid returned by pnpm is wrong and would not shut down the server 
-TEST_PORT=3005 node --import tsx test/server.ts & 
+TEST_PORT=3005 "$(pnpm bin)/tsx" test/server.ts & 
 SERVER_PID=$! &&
 sleep 2 && # Arbitrary time
 npm run test:fixtures
