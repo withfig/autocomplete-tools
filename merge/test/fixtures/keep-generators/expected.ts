089baa5a007f669656e9d51a624cb2829224a17e
Grant Gurvis <grangurv@amazon.com>
2024-05-31 11:53:16 -0700

    chore: Update dependencies

--- merge/test/fixtures/keep-generators/expected.ts
+++ merge/test/fixtures/keep-generators/expected.ts
@@ -1,3 +1,15 @@
+const generator: Fig.Generator = {
+  custom: async (tokens) => [
+    {
+      name: tokens[0],
+    },
+  ],
+};
+const generatorMap = {
+  foo: {
+    script: ["foo"],
+  } as Fig.Generator,
+};
 const spec: Fig.Spec = {
   name: "foo",
   description: "An updated description",
@@ -7,12 +19,31 @@ const spec: Fig.Spec = {
       args: {
         name: "push",
         generators: {
-          script: "foo",
-          postProcess: (out) => [],
+          script: ["foo"],
+          postProcess: (out) => [
+            {
+              name: out,
+            },
+          ],
         },
       },
     },
   ],
-  subcommands: [{ name: "bar", displayName: "barbar" }, { name: "foo" }],
+  subcommands: [
+    {
+      name: "bar",
+      args: { name: "baz", generators: generator },
+      displayName: "barbar",
+    },
+    {
+      name: "foo",
+      options: [
+        {
+          name: "-a",
+          args: { name: "push", generators: generatorMap.foo },
+        },
+      ],
+    },
+  ],
 };
 export default spec;
