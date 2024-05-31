f22eaaeb3979d51b7053b42a67a89064b46eb2f7
Grant G <grant0417@users.noreply.github.com>
2024-05-31 14:55:45 -0400

    chore: Update dependencies (#148)

--- merge/test/fixtures/keep-generators/new.ts
+++ merge/test/fixtures/keep-generators/new.ts
@@ -2,7 +2,18 @@ const spec: Fig.Spec = {
   name: "foo",
   description: "An updated description",
   options: [{ name: "-a", args: { name: "push" } }],
-  subcommands: [{ name: "bar" }, { name: "foo" }],
+  subcommands: [
+    { name: "bar", args: { name: "baz" } },
+    {
+      name: "foo",
+      options: [
+        {
+          name: "-a",
+          args: { name: "push" },
+        },
+      ],
+    },
+  ],
 };
 
 export default spec;
--- merge/test/fixtures/keep-generators/old.ts
+++ merge/test/fixtures/keep-generators/old.ts
@@ -1,3 +1,17 @@
+const generator: Fig.Generator = {
+  custom: async (tokens) => [
+    {
+      name: tokens[0],
+    },
+  ],
+};
+
+const generatorMap = {
+  foo: {
+    script: ["foo"],
+  } as Fig.Generator,
+};
+
 const spec: Fig.Spec = {
   name: "foo",
   description: "A simple description",
@@ -7,13 +21,32 @@ const spec: Fig.Spec = {
       args: {
         name: "push",
         generators: {
-          script: "foo",
-          postProcess: (out) => [],
+          script: ["foo"],
+          postProcess: (out) => [
+            {
+              name: out,
+            },
+          ],
         },
       },
     },
   ],
-  subcommands: [{ name: "bar", displayName: "barbar" }, { name: "foo" }],
+  subcommands: [
+    {
+      name: "bar",
+      displayName: "barbar",
+      args: { name: "baz", generators: generator },
+    },
+    {
+      name: "foo",
+      options: [
+        {
+          name: "-a",
+          args: { name: "push", generators: generatorMap.foo },
+        },
+      ],
+    },
+  ],
 };
 
 export default spec;
